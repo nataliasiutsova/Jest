9c751921f66db4fc43f69ce1cf3dbf6d8da6cc2c
nataliasiutsova <tashaklimenkova@gmail.com>
2023-02-14 00:19:37 +0300

    Added code coverage

--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,9 @@ const { calculateBonus } = require('../index.js');
 
 describe('calculateBonus function:', () => {
   test(' should return correct bonus', () => {
-    expect(calculateBonus(20, 10)).toBeLessThan(50);
-    expect(calculateBonus(30, 30)).toEqual(50);
+    expect(calculateBonus(20, 29)).toBeLessThan(50);
     expect(calculateBonus(10, 40)).toEqual(50);
+    expect(calculateBonus(20, 31)).toEqual(50);
   });
   test(' should return Nan if function arguments not defined', () => {
     expect(calculateBonus()).toBeNaN();
